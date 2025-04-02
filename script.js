ded6a968c4ebe6a58d9912e0a741da4adc0d2058
Javi <javier_diazg@my.uvm.edu.mx>
2025-04-02 18:53:59 +0000

    Agregando script.js de la marquesina

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-// Seleccionamos el texto de la marquesina
 const texto = document.getElementById("textoMarquesina");
+let posicion = window.innerWidth;
 
-// Posición inicial
-let posicion = 100;
-
-// Función que mueve el texto hacia la izquierda
 function moverTexto() {
   posicion--;
   texto.style.left = posicion + "px";
 
-  // Si el texto ya no se ve, lo reiniciamos desde la derecha
   if (posicion < -texto.offsetWidth) {
     posicion = window.innerWidth;
   }
 
-  // Llamamos a la función otra vez de forma continua
   requestAnimationFrame(moverTexto);
 }
 
-// Iniciamos la animación cuando el archivo carga
 moverTexto();
